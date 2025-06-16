5e23f841df07bc88027f95aa1ac57ef6648163e4
Andriamahaly Loiqua <hei.loiqua@gmail.com>
2025-06-16 23:35:48 +0300

    feat: add and configure seed script for test users

---
+++ prisma/seed.ts
@@ -0,0 +1,44 @@
+import { PrismaClient } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  console.log('Start seeding ...');
+
+  const password = await bcrypt.hash('password123', 12);
+
+  const alice = await prisma.user.upsert({
+    where: { email: 'alice@prisma.io' },
+    update: {},
+    create: {
+      email: 'alice@prisma.io',
+      username: 'alice',
+      name: 'Alice',
+      passwordHash: password,
+    },
+  });
+
+  const bob = await prisma.user.upsert({
+    where: { email: 'bob@prisma.io' },
+    update: {},
+    create: {
+      email: 'bob@prisma.io',
+      username: 'bob',
+      name: 'Bob',
+      passwordHash: password,
+    },
+  });
+
+  console.log({ alice, bob });
+  console.log('Seeding finished.');
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
